cc93dabc06157a356c17abe10a8af15663b18ae8
Prakki Rama <ramadatta.88@gmail.com>
2023-10-18 12:14:34 +0200

    Update parsebio_installation_cmds.sh

--- parsebio_installation_cmds.sh
+++ parsebio_installation_cmds.sh
@@ -7,10 +7,13 @@ pip install --no-cache-dir ./
 ./split-pipe -h
 mkdir genomes
 cd genomes/
+# Download genomes
 wget https://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz;
 wget https://ftp.ensembl.org/pub/release-109/gtf/homo_sapiens/Homo_sapiens.GRCh38.109.gtf.gz;
 wget https://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz;
 wget https://ftp.ensembl.org/pub/release-109/gtf/mus_musculus/Mus_musculus.GRCm39.109.gtf.gz;
+
+
 split-pipe \
 --mode mkref \
 --genome_name hg38 \
